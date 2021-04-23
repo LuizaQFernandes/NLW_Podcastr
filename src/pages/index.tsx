b38f927a7a39db494b5b27dfef4e9ab052b82c82
Luiza Fernandes <luhqfernandes@gmail.com>
2021-04-23 16:14:16 -0300

    audio included - Context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import { convertDurationToTimeString } from '../utils/convertDurationToTimeStrin
 import Link from 'next/link';
 
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 type Episode =  {
   id: string;
@@ -25,10 +27,13 @@ type HomeProps =  {
 }
 
 export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
+  
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
-          <h2> Últimos lançamentos</h2>
+          <h2> Últimos lançamentos </h2>
 
           <ul>
               {latestEpisodes.map(episode => {
@@ -52,7 +57,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
 
                     </div>
 
-                    <button type="button">
+                    <button type="button" onClick={() => play(episode)}>
                       <img src="/play-green.svg" alt="Tocar episódio"/>
                     </button>  
                   </li>
